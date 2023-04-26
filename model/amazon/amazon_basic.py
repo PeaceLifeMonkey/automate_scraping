b0937fd673b05a845907226f2d1d91f5ee6f13b7
PeaceLifeMonkey <qetar0304@gmail.com>
2023-04-27 00:43:40 +0700

    add get_others_info function

--- model/amazon/amazon_basic.py
+++ model/amazon/amazon_basic.py
@@ -1,7 +1,9 @@
 import timeit
 import re
+import pandas as pd
 import polars as pl
 import json
+import datetime
 
 from bs4 import BeautifulSoup
 from common.utils.amazon.amazon import Amazon
@@ -10,19 +12,6 @@ from common.utils.amazon.amazon import Amazon
 class ScrapAmazon(Amazon):
     def __init__(self, config_path, profile_path) -> None:
         super().__init__(config_path, profile_path)
-        self.parent_asin = str()
-        self.title = str()
-        self.main_image = str()
-        self.rating = int()
-        self.rating_count = float()
-        self.new_price = float()
-        self.list_price = float()
-        self.brand = str()
-        self.color = str()
-        self.material = str()
-        self.frame_material = str()
-        self.top_material = str()
-        self.remarks = list()
 
     def timeit_decorator(func):
         def wrapper(*args, **kwargs):
@@ -49,10 +38,46 @@ class ScrapAmazon(Amazon):
         self.frame_material = str()
         self.top_material = str()
         self.remarks = list()
+        self.rank_root = str()
+        self.rank_root_no = int()
+        self.rank_sub = str()
+        self.manufacturer = str()
+        self.date_first_available = datetime.datetime(2010, 10, 10)
+        self.df_offers = pd.DataFrame()
+        self.offers_count = int()
 
     @timeit_decorator
     def scrap_amazon(self, asin:str, zipcode:str) -> None:
         self.init_variable()
+
+        self.result = pd.DataFrame(
+            columns=[
+                "asin",
+                "product_title",
+                "color",
+                "material",
+                "frame_material",
+                "top_material",
+                "brand",
+                "parent_asin",
+                "variation",
+                "cats_1",
+                "cats_2",
+                "cate_id_root",
+                "asin_status",
+                "list_price",
+                "new_price",
+                "main_img",
+                "manufacturer",
+                "date_first_available",
+                "rating",
+                "rank_root",
+                "rank_sub",
+                "rating_count",
+                "rank_root_no",
+                "manufacturer"
+            ]
+        )
 
         self.set_amazon_location_v2(asin,zipcode)
         self.get_basic_info(asin)        
@@ -62,20 +87,22 @@ class ScrapAmazon(Amazon):
             self.driver.implicitly_wait(0.5)
             page_source = self.driver.page_source
             soup = BeautifulSoup(page_source,'html.parser')
+
             self.get_image(soup)
             self.get_category(soup)
-            self.get_price(asin)
             self.get_title(soup)
             self.get_weight(soup)
-            self.get_rating(soup)
             self.get_brand(soup)
             self.get_material(soup)
             self.get_color(soup)
             self.get_parent_variations(page_source)
+            self.get_others_info(soup)
+            if self.asin_status != "Currently unavailable.":
+                self.get_price(asin,soup)
 
     def get_others_info(self, soup:BeautifulSoup) -> None:
-        eles_rating = self.driver_find_elements(
-            '//*[@id="averageCustomerReviews_feature_div"]/div[@id="averageCustomerReviews"]'
+        eles_rating = soup.find(
+            "div",{"id":"averageCustomerReviews_feature_div"}
         )
         eles_feature = self.driver_find_elements(
             '//*[@id="detailBullets_feature_div"]/ul/li/span/span[1]'
@@ -88,14 +115,14 @@ class ScrapAmazon(Amazon):
         )
 
         if (
-            len(eles_rating) == 0
+            eles_rating is None
             and len(eles_feature) == 0
             and len(eles_section_1) == 0
             and len(eles_techspec) == 0
         ):
             self.remarks = self.remarks + ["[3. Not Found other info]"]
         else:
-            if len(eles_rating)> 0:
+            if eles_rating is not None:
                 self.get_rating(eles_rating)
             if len(eles_feature) > 0:
                 self.get_info_from_feature(eles_feature)
@@ -107,6 +134,71 @@ class ScrapAmazon(Amazon):
             numbers = re.findall('[0-9,]+', self.rank_root)
             if len(numbers) != 0:
                 self.rank_root_no = int(numbers[0].replace(',', ''))
+
+    def get_info_from_feature(self, eles_feature) -> None:
+        li_count = 0
+        list_collected = []
+
+        for ele in eles_feature:
+            li_count = li_count + 1
+            if ele.text in list_collected:
+                break
+            list_collected = list_collected + [ele.text]
+
+            # if ele.text == 'ASIN :':
+            #     data_if['PARENT_ASIN'] = self.driver_find_element('//*[@id="detailBullets_feature_div"]/ul/li['+str(li_count)+']/span/span[2]').text
+            #     parent_asin = data_if['PARENT_ASIN']
+            if ele.text == "Manufacturer :":
+                self.manufacturer = self.driver_find_element(
+                    '//*[@id="detailBullets_feature_div"]/ul/li['
+                    + str(li_count)
+                    + "]/span/span[2]"
+                ).text
+            elif ele.text == "Date First Available :":
+                date_first_avai_text = self.driver_find_element(
+                    '//*[@id="detailBullets_feature_div"]/ul/li['
+                    + str(li_count)
+                    + "]/span/span[2]"
+                ).text
+                # self.date_first_available = dateparser.parse(
+                #     date_first_avai_text, date_formats=["%B %d, %Y"]
+                # )
+                self.date_first_available = datetime.datetime.strptime(
+                    date_first_avai_text, "%B %d, %Y"
+                )
+
+        try:
+            rank_text = self.driver_find_element(
+                '//*[@id="detailBulletsWrapper_feature_div"]/ul[@class="a-unordered-list a-nostyle a-vertical a-spacing-none detail-bullet-list"]/li/span'
+            ).text
+            root_text = (
+                rank_text[: rank_text.find("(")]
+                .strip()
+                .replace("Best Sellers Rank: ", "")
+            )
+            self.rank_root = (
+                root_text.replace("(", "").strip().replace("&", "and").replace("#", "")
+            )
+            sub_text = rank_text[rank_text.find(")") + 1 :].strip()
+            self.rank_sub = (
+                sub_text.replace("(", "").strip().replace("&", "").replace("#", "")
+            )
+        except:
+            pass
+        if self.rating == float(0) and self.rating_count == 0:
+            try:
+                rating_text = self.driver_find_element(
+                    '//*[@id="acrPopover"]/span[@class="a-declarative"]/a/i[1]/span'
+                ).get_attribute("innerHTML")
+                count_rating_text = self.driver_find_element(
+                    '//*[@id="acrCustomerReviewText"]'
+                ).text
+                self.rating = float(rating_text[: rating_text.find(" out")])
+                self.rating_count = int(
+                    count_rating_text[: count_rating_text.find(" rating")].replace(",", "")
+                )
+            except:
+                pass
 
     def get_asin_status(self, soup:BeautifulSoup) -> None:
         status_element = soup.find('div',{"id":"availability"})
@@ -229,73 +321,62 @@ class ScrapAmazon(Amazon):
         else:
             self.remarks = self.remarks + ["[7. Not Found pic]"]
 
-    def get_price(self, asin:str) -> None:
-        list_price = self.driver_find_elements(
-            '//div[@id="corePriceDisplay_desktop_feature_div"]'
-            '/div[@class="a-section a-spacing-small '\
-            'aok-align-center"]/span/'
-            'span[@class="a-size-small a-color-secondary '\
-            'aok-align-center basisPrice"]/'
-            'span[@class="a-price a-text-price"]'
-        )
-        
-        if len(list_price) != 0:
-            price_data = list_price[0].text
+    def get_price(self, asin:str, soup:BeautifulSoup) -> None:
+        list_price = soup.find("div",{"id":"corePriceDisplay_desktop_feature_div"})
+        if list_price is not None:
+            list_price = list_price.find("div",{"class":"a-section "\
+            "a-spacing-small aok-align-center"}).find("span").find(
+                "span",{"class":"a-size-small "\
+                "a-color-secondary aok-align-center basisPrice"
+            }).find("span",{"class":"a-price a-text-price"})
+
+        if list_price is not None:
+            price_data = list_price.text.strip()
             list_match = re.findall(r"[$][0-9.,]*", price_data)
             if len(list_match) != 0:
                 self.list_price = float(list_match[0].replace('$', '').replace(',', '').strip())
         else:
-            elements = self.driver_find_elements('//div[@id="corePrice_desktop"]//tr')
+            elements = soup.find('div',{"id":"corePrice_desktop"}).find_all("tr")
             list_price = None
             if len(elements) == 2:
                 # Fix for exception have "Save money"
-                if 'You Save' in elements[1].text:
-                    list_price = elements[0].text
+                if 'You Save' in elements[1].text.strip():
+                    list_price = elements[0].text.strip()
                     list_match = re.findall(r"[$][0-9.,]*", list_price)
                     if len(list_match) != 0:
                         self.list_price = float(list_match[0].replace('$', '').replace(',', '').strip())
 
-        whole_price_element = self.driver_find_elements(
-            '//*[@class="a-price aok-align-center '\
-            'reinventPricePriceToPayMargin priceToPay"]/'
-            'span[@aria-hidden="true"]/span[@class="a-price-whole"]'
-        )
-        if len(whole_price_element) != 0:
-            whole_price_text = whole_price_element[0].text
-            frac_price_element = self.driver_find_elements(
-                '//*[@class="a-price aok-align-center '\
-                'reinventPricePriceToPayMargin priceToPay"]/'
-                'span[@aria-hidden="true"]/'\
-                'span[@class="a-price-fraction"]'
-            )
-            if len(frac_price_element) != 0:
-                frac_price_text = frac_price_element[0].text
+        newprice_element = soup.find("span",{"class":"a-price "\
+            "aok-align-center "\
+            "reinventPricePriceToPayMargin priceToPay"
+        })
+        if newprice_element is not None:
+            newprice_str:str = newprice_element.find("span",
+            {"class":"a-offscreen"})
+            if newprice_str is not None:
+                self.new_price = float(
+                    newprice_str.text.strip().replace("$","")
+                )
             else:
-                frac_price_text = 0
-            
-            whole_price_text = whole_price_text.replace(',', '').replace('.', '')
-            if whole_price_text == '':
-                whole_price_text = '0'
-            if frac_price_text == '':
-                frac_price_text = '0'
-
-            self.amz_price = float(whole_price_text + "." + frac_price_text)
+                self.new_price = float(0)
         else:
-            self.amz_price = 0
+            self.new_price = float(0)
 
-        if self.amz_price == 0:
-            elements = self.driver_find_elements(f'//*[@data-asin="{asin}"]//*[@id="_price"]')
+        if self.new_price == 0:
+            elements = soup.find_all(f'//*[@data-asin="{asin}"]//*[@id="_price"]')
+            elements = soup.find_all(attrs={"data-asin":asin})
             if len(elements) != 0:
-                price_data = elements[0].text
-                list_match = re.findall(r"[$][0-9.,]*", price_data)
-                if len(list_match) != 0:
-                    self.amz_price = float(list_match[0].replace('$', '').replace(',', '').strip())
+                for ele in elements:
+                    price_data = ele.find(attrs={"id":"_price"})
+                    if price_data is not None:
+                        price_data = price_data.text.strip()
+                        list_match = re.findall(r"[$][0-9.,]*", price_data)
+                        if len(list_match) != 0:
+                            self.new_price = float(list_match[0].replace('$', '').replace(',', '').strip())
 
-        check_free_txt = self.driver_find_elements_by_id(
-            'actualPriceValue'
-        )
+        check_free_txt = soup.find_all({"id":"actualPriceValue"})
         if len(check_free_txt) != 0:
-            check_free_txt = check_free_txt[0].text
+            check_free_txt = check_free_txt[0].text.strip()
             if check_free_txt == 'Free Download':
                 self.list_price = 0
             self.remarks = self.remarks + [check_free_txt]
@@ -392,14 +473,15 @@ class ScrapAmazon(Amazon):
         else:
             self.title = None
 
-    def get_rating(self, soup:BeautifulSoup) -> None:
-        eles_1 = soup.find_all("div",{"id":"averageCustomerReviews_feature_div"})
-        if len(eles_1) != 0:
-            rating = self.driver_find_elements('//*[@id="acrPopover"]')
+    def get_rating(self, eles_rating:BeautifulSoup) -> None:
+        eles_1 = eles_rating.find("div",{"id":"averageCustomerReviews"})
+        if eles_1 is not None:
+            rating = eles_1.find(attrs={"id":"acrPopover"})
             if len(rating) != 0:
-                rating = rating[0].get_attribute('title')
+                rating = rating['title']
                 self.rating = float(rating.replace(' out of 5 stars',''))
-                rating_count = eles_1[0].find("span",{"id":"acrCustomerReviewText"}).text.strip()
+                rating_count = eles_1.find("span",
+                {"id":"acrCustomerReviewText"}).text.strip()
                 self.rating_count = int(
                     rating_count.replace(
                     ' ratings','').strip().replace(',','')
@@ -419,10 +501,10 @@ class ScrapAmazon(Amazon):
 
 
 if __name__ == "__main__":
-    profile_path = '/media/quang/Quang/Automate/profiles'
+    profile_path = '/media/quang/Quang/Automate/profiles/web_amz'
     cfg_path = '/media/quang/Quang/Automate/configs/amazon/amazon_web_config.yaml'
     amz = ScrapAmazon(cfg_path,profile_path)
-    asin = 'B07F1ZB48H'
+    asin = 'B0731DFTR2'
     # amz.set_amazon_location_v2(asin,'90001')
     amz.scrap_amazon(asin,'90001')
     pass
